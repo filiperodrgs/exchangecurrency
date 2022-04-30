5213c3a61f52a5087eeb27cd644ccc90fdbebf60
Filipe Rodrigues <filiperodrigues@mbp-de-filipe.home>
2022-04-30 09:25:45 +0100

    beggining version friday

---
+++ src/test/java/com/example/ExchangeCurrency/ExchangeCurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ExchangeCurrency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeCurrencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
